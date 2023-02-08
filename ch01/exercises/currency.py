503cb024561d66be9041f820fc24e9caa83f2fa5
gracepodgurski <gracepodgurski@yahoo.com>
2023-02-08 16:53:04 -0500

    adjustments

--- ch01/exercises/currency.py
+++ ch01/exercises/currency.py
@@ -1,7 +1,7 @@
 #Currency conversion
 
-rate = float(input("Enter current exchange rate from Euro to US Dollar"))
-amount =  float(input("Enter amount of currency you would like to exchange"))
+rate = float(input("Enter current exchange rate from Euro to US Dollar:  "))
+amount =  float(input("Enter amount of currency you would like to exchange:  "))
 
 usd = amount * rate
 result = usd - 3.00
