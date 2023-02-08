565e30b8c94179b022036e9a75e890a053c7efd7
gracepodgurski <gracepodgurski@yahoo.com>
2023-02-08 16:41:00 -0500

    drawing

---
+++ ch02/exercises/drawing.py
@@ -0,0 +1,11 @@
+
+sides = input("Please enter the number of sides")
+length = input("Please enter the length of the sides")
+angle = 360 / sides
+
+import turtle
+turtle.forward(length)
+turtle.right(angle)
+
+
+
