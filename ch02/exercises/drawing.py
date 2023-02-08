3e2a9ec5b423b2a34516165d708b522e1a529f48
gracepodgurski <gracepodgurski@yahoo.com>
2023-02-08 17:35:54 -0500

    needs more

--- ch02/exercises/drawing.py
+++ ch02/exercises/drawing.py
@@ -1,11 +1,20 @@
 
-sides = input("Please enter the number of sides")
-length = input("Please enter the length of the sides")
+sides = int(input("Please enter the number of sides:  "))
+length = int(input("Please enter the length of the sides:  "))
 angle = 360 / sides
 
+
+import math
 import turtle
+pen = turtle.Turtle()
 turtle.forward(length)
 turtle.right(angle)
+window = turtle.Screen()
+
+window.exitonclick()
+
+# This code needs more work, but not required in class on 2/8
+
 
 
 
