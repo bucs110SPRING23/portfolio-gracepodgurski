47a3c9396e8c03ba1298c6353c8e4be790bb518b
gracepodgurski <gracepodgurski@yahoo.com>
2023-02-13 17:54:26 -0500

    shapes updated

--- ch02/exercises/shapes.py
+++ ch02/exercises/shapes.py
@@ -3,15 +3,17 @@ pygame.init()
 
 screen = pygame.display.set_mode()
 
+pygame.draw.circle(screen, "white", [200,350], 100)
+pygame.display.flip()
+pygame.time.wait(2000)
+
+pygame.draw.circle(screen, "white", [200,250], 75)
+pygame.display.flip()
+pygame.time.wait(2000)
 
 pygame.draw.circle(screen, "white", [200,150], 50)
 pygame.display.flip()
 pygame.time.wait(2000)
 
-pygame.draw.circle(screen, "white", [200,250], 78)
-pygame.display.flip()
-pygame.time.wait(2000)
 
-pygame.draw.circle(screen, "white", [200,350], 100)
-pygame.display.flip()
-pygame.time.wait(2000)
+
