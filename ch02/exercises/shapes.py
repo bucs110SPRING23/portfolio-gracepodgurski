717128ff894c21445267950189243983c4757078
gracepodgurski <gracepodgurski@yahoo.com>
2023-02-15 17:09:53 -0500

    game!

--- ch02/exercises/shapes.py
+++ ch02/exercises/shapes.py
@@ -3,17 +3,34 @@ pygame.init()
 
 screen = pygame.display.set_mode()
 
-pygame.draw.circle(screen, "white", [300,350], 100)
+dimensions = screen.get_size()
+print(dimensions)
+starting_point = [dimensions[0]//2, dimensions[1]//2]
+
+
+radius = 200 
+for _ in range(3):
+    pygame.draw.circle(screen, "white", starting_point, radius)
+    starting_point[1] = starting_point[1] - radius
+    radius = radius//2
+    starting_point[1] = starting_point[1] - radius
+    
 pygame.display.flip()
 pygame.time.wait(2000)
 
-pygame.draw.circle(screen, "white", [300,250], 75)
-pygame.display.flip()
-pygame.time.wait(2000)
 
-pygame.draw.circle(screen, "white", [300,150], 50)
-pygame.display.flip()
-pygame.time.wait(2000)
+
+#pygame.draw.circle(screen, "white", starting_point, 100)
+#pygame.display.flip()
+#pygame.time.wait(2000)
+
+#pygame.draw.circle(screen, "white", second_point, 75)
+#pygame.display.flip()
+#pygame.time.wait(2000)
+
+#pygame.draw.circle(screen, "white", third_point, 50)
+#pygame.display.flip()
+#pygame.time.wait(2000)
 
 
 
