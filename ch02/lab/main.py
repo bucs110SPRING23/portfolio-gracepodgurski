d05f45150488085f2ea81e9e0ccf0473c7d0d269
gracepodgurski <gracepodgurski@yahoo.com>
2023-02-15 09:39:55 -0500

    edits

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -1,5 +1,20 @@
 #Part A
 
+import turtle 
 import random
-x = random.range(1,10)
-print(x)
+don = random.randrange(1,100)
+mic = random.randrange(1,100)
+
+window = turtle.Screen()
+
+pen1 = turtle.Turtle()
+pen1.color("black")
+pen1.goto(-100, 20)
+
+pen2 = turtle.Turtle()
+pen2.color("red")
+pen2.goto(-100,-20)
+
+
+
+window.exitonclick()
