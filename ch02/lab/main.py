3cbe74f98a64ab684825e25eb37caa32302929c0
gracepodgurski <gracepodgurski@yahoo.com>
2023-02-15 11:31:12 -0500

    edits

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -7,14 +7,30 @@ mic = random.randrange(1,100)
 
 window = turtle.Screen()
 
-pen1 = turtle.Turtle()
-pen1.color("black")
-pen1.goto(-100, 20)
+#Race 1
+#pen1 = turtle.Turtle()
+#pen1.color("black")
+#pen1.goto(-100, 20)
+#pen1.forward(don)
 
-pen2 = turtle.Turtle()
-pen2.color("red")
-pen2.goto(-100,-20)
+#pen2 = turtle.Turtle()
+#pen2.color("red")
+#pen2.goto(-100,-20)
+#pen2.forward(mic)
 
+#window.exitonclick()
+
+
+#Race 2
+raph = float(random.randrange(1,10))
+leo = float(random.randrange(1,10))
+
+for s in range(myturtles):
+    pen = turtle.Turtle()
+    pen.color("black")
+    pen.goto(-100, 20)
+    pen.forward(myturtles)
 
 
 window.exitonclick()
+
