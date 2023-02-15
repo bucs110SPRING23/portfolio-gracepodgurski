16430d5ea5ad8e4c2e6e8ff286c8a3186024bd14
gracepodgurski <gracepodgurski@yahoo.com>
2023-02-15 17:21:50 -0500

    newfile

---
+++ ch03/exercises/examples.py
@@ -0,0 +1,19 @@
+#In Class Examples
+#bool - boolean value 
+    #True/False
+    # *** -caps are important 
+    #True, 1, "Hello"
+
+print(type(True))
+print(bool(1), bool(-1), bool("hello"))
+
+#boolean expression
+
+x  = 5
+y = 10
+
+print(x == y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
