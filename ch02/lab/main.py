729aa67ed77f1c1ccc7b437b1be84326544fed41
gracepodgurski <gracepodgurski@yahoo.com>
2023-02-20 22:46:13 -0500

    submission but not complete

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -1,61 +1,60 @@
 #Part A
 
-# import turtle 
-# import random
+import turtle 
+import random
+import pygame
+import math
+window = turtle.Screen()
 
-# window = turtle.Screen()
+#Race 1
+don = turtle.Turtle()
+don.color("black")
+don.shape("turtle")
+don.up()
 
-# #Race 1
-# don = turtle.Turtle()
-# don.color("black")
-# don.shape("turtle")
-# don.up()
+mic = turtle.Turtle()
+mic.color("red")
+mic.shape("turtle")
+mic.up()
 
-# mic = turtle.Turtle()
-# mic.color("red")
-# mic.shape("turtle")
-# mic.up()
+don.goto(-100,20)
+mic.goto(-100,-20)
+don.down()
+mic.down()
+don.forward(random.randrange(1,100))
+mic.forward(random.randrange(1,100))
 
-# don.goto(-100,20)
-# mic.goto(-100,-20)
-# don.down()
-# mic.down()
-# don.forward(random.randrange(1,100))
-# mic.forward(random.randrange(1,100))
-
-# window.exitonclick()
+window.exitonclick()
 
 #Race 2
-# raph = turtle.Turtle()
-# raph.color("blue")
-# raph.shape("turtle")
-# raph.up()
-# raph.goto(-100,20)
+raph = turtle.Turtle()
+raph.color("blue")
+raph.shape("turtle")
+raph.up()
+raph.goto(-100,20)
 
-# leo = turtle.Turtle()
-# leo.color("green")
-# leo.shape("turtle")
-# leo.up()
-# leo.goto(-100,-20)
+leo = turtle.Turtle()
+leo.color("green")
+leo.shape("turtle")
+leo.up()
+leo.goto(-100,-20)
 
-# for s in range(0,10):
-#     raph.down()
-#     raph.forward(int(random.randrange(1,10)))
-#     leo.down()
-#     leo.forward(int(random.randrange(1,10)))
+for s in range(0,10):
+    raph.down()
+    raph.forward(int(random.randrange(1,10)))
+    leo.down()
+    leo.forward(int(random.randrange(1,10)))
 
-# raph.up()
-# raph.goto(-100,20)
-# leo.up()
-# leo.goto(-100,-20)
+raph.up()
+raph.goto(-100,20)
+leo.up()
+leo.goto(-100,-20)
 
-# window.time.wait(20000)
-# window.exitonclick()
+window.time.wait(20000)
+window.exitonclick()
 
 #Part B - Drawing Shapes 
-
-import pygame 
-import math
+#As of 2/20, I'm still trying to figure out this part
 pygame.init()
 window1 = pygame.display.set_mode()
 # Sides:
