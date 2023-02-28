3b342c6c5f3f66d54e03e5921228ea797b715573
gracepodgurski <gracepodgurski@yahoo.com>
2023-02-27 21:03:38 -0500

    yep

--- ch04/exercises/guess.py
+++ ch04/exercises/guess.py
@@ -2,22 +2,33 @@ import random
 guess = int(input("Please enter a number between 1-1000: "))
 x = random.randrange(1,10)
 
+count = []
 
 while guess != x:
-    # for count, value in enumerate(values, start=1):
-    #  print(count, value)
+    n = 1
     if guess < x:
         print("Too Low!")
+        n = n + 1
+        count.append([n])
         guess = int(input("Please enter a number between 1-1000: "))
     elif guess > x:
         print("Too High!")
+        n = n + 1
+        count.append([n])
         guess = int(input("Please enter a number between 1-1000: "))
-    elif guess == x: 
-        print("correct!!")
-    break
+
+if guess == x: 
+    print("correct!!")
 
 print(x)
+print(count)
 
-# for count, value in enumerate(values, start=1):
-#         print(count, value)
+# still trying to figure out how to do the enumerate thing...
 
+    # def my_enumerate(guess, start=0):
+    #  n = start
+    #  for elem in sequence:
+    #      yield n, elem
+    #      n += 1
+    # for count, value in enumerate(values, start=1):
+    #  print(count, value)
