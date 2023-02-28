e994d02d4560801f04e5d7c2ce31b0bdd88dc20c
gracepodgurski <gracepodgurski@yahoo.com>
2023-02-27 21:20:54 -0500

    ch ch ch ch changesss

--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -2,17 +2,24 @@ import pygame
 import random
 pygame.init()
 
-screen = pygame.display.set_mode()
-
 while 1:
     pygame.event.pump()
-
+    screen = pygame.display.set_mode()
+    screen.fill ("white")
+    dimensions = screen.get_size()
+    print(dimensions)
+    center = [dimensions[0]//2, dimensions[1]//2]
     dimensions = screen.get_size()
     print(dimensions)
     starting_point = [dimensions[0]//2, dimensions[1]//2]
 
+    pygame.draw.circle(screen,"yellow",center, 525)
 
+    # distance_from_center = math.hypot(x1-x2, y1-y2) #the distance formula
+    # is_in_circle = distance_from_center <= width/2 #screen width
 
-    pygame.time.wait(2000)
+    pygame.time.wait(5000)
+
     break 
 
+# Still working on this... I have been sick so I  am still trying to catch up  with this lab and the previous one
