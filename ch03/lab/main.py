517ac90ec25aaea2bb05cb931c5da072c3190bb1
gracepodgurski <gracepodgurski@yahoo.com>
2023-03-02 22:10:55 -0500

    dartboard completed!

--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -5,16 +5,20 @@ pygame.init()
 
 while 1:
     pygame.event.pump()
-    screen = pygame.display.set_mode()
+    screen = pygame.display.set_mode((1050,1050))
     screen.fill("white")
     dimensions = screen.get_size()
-    print(dimensions)
 
     center = [dimensions[0]//2, dimensions[1]//2]
     starting_point = [dimensions[0]//2, dimensions[1]//2]
 
-    pygame.draw.circle(screen,"yellow",center, 525)
+    pygame.draw.circle(screen,"black",center, 525)
+    pygame.draw.circle(screen,"orange",center, 524)
+    pygame.draw.line(screen,"black",(dimensions[0]//2,0),(dimensions[0]//2,dimensions[1]))
+    pygame.draw.line(screen,"black",(0,dimensions[1]//2),(dimensions[0],dimensions[1]//2,))
+    
     pygame.display.flip()
+
 
     # distance_from_center = math.hypot(x1-x2, y1-y2) #the distance formula
     # is_in_circle = distance_from_center <= width/2 #screen width
