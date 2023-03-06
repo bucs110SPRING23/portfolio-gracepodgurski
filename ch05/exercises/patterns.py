59aa0bd09168267df0808d85273dd66b43a087e7
gracepodgurski <gracepodgurski@yahoo.com>
2023-03-06 17:59:42 -0500

    pushin it to save!

--- ch05/exercises/patterns.py
+++ ch05/exercises/patterns.py
@@ -1,7 +1,9 @@
-star = input("Please input your desired number of rows: ")
+star = int(input("Please input your desired number of rows: "))
 
 def starpyramid():
-    for s in range(star):
-        print(star*"*")
+    for s in range(1,star):
+        symbol = "*"
+        print(symbol*star)
 
 
+starpyramid()
