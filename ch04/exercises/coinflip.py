af4645e005ad120ad97da0bb868b23ac78445bb5
gracepodgurski <gracepodgurski@yahoo.com>
2023-03-06 17:03:21 -0500

    hello

---
+++ ch04/exercises/coinflip.py
@@ -0,0 +1,34 @@
+import turtle
+import random
+
+window = turtle.Screen()
+
+player1 = turtle.Turtle()
+player1.color("green")
+player1.shape("turtle")
+player1.speed(0)
+
+distance = 10
+angle = 90
+in_screen = True 
+
+while in_screen:
+    coin = random.randrange(0,2)
+    if coin:
+        player1.right(angle)
+    else:
+        player1.left(angle)
+    player1.forward(distance)
+
+    turtleX = player1.xcor()
+    turtleY = player1.ycor()
+
+    x_range = window.window_width()  / 2
+    y_range = window.window_height() / 2
+
+    if abs(turtleX) > x_range or abs(turtleY) > y_range:
+        in_screen = False
+
+
+
+
