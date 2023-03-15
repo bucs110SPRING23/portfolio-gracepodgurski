145219a79f0d8457f43e18d4477fde9aa3771e27
gracepodgurski <gracepodgurski@yahoo.com>
2023-03-15 17:55:47 -0400

    class notes!

--- ch06/exercises/notes.py
+++ ch06/exercises/notes.py
@@ -1,2 +1,36 @@
 # Files
 #  - saved program state
+
+# def main():
+#     idea = input("Enter an idea: ")
+#     ideas = []
+#     ideas.append(idea)
+#     print(ideas)
+
+# main()
+
+#  When the program ends, all program data is deleted. If we want to save this, we use files
+
+# Operating System: - manage files
+# request the file from OS
+#  - where (assets)
+#  - name (ideas.txt)
+#  - how to use it (r = read mode)(w = writes)(a = appends)
+#  when open for writing, the file is deleted and restarted (if file doesnt exist, it will create it)
+
+# - working with files is one-way
+
+def main():
+    file_pointer = open("assets/ideas.txt","r")
+
+    ideas = file_pointer.read()
+    print(ideas)
+
+
+# charcater for a line break "\n"
+    for i in ideas:
+        file_pointer.write(i+"\n")
+
+        
+main()
+
