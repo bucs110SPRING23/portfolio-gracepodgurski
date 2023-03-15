5e7999d907c51e151472b994cfeb9c96f0556ae2
gracepodgurski <gracepodgurski@yahoo.com>
2023-03-15 17:10:13 -0400

    pushin

--- ch05/exercises/scope.py
+++ ch05/exercises/scope.py
@@ -1,4 +1,24 @@
-# Started but need to finish next class 
 
-def addition(num1,num2):
-    sum = num1 + num2
+def multiply(num1,num2):
+    mult = 0
+    for _ in range(num2):
+        mult = mult + num1
+    return mult
+
+def exponent(x1,x2):
+    exp = 1
+    for _ in range(x2):
+        exp = exp * x1
+    return exp 
+
+def square(x):
+    return exponent(x,2)
+
+
+#  ---- MAIN CODE ------
+def main():
+    print(multiply(10,3)) 
+    print(exponent(3,3))
+    print(square(5))
+
+main()
