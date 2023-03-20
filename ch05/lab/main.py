0482530e32e509c19cf7ef153017042efedecfb4
gracepodgurski <gracepodgurski@yahoo.com>
2023-03-20 09:44:33 -0400

    saving

---
+++ ch05/lab/main.py
@@ -0,0 +1,13 @@
+def iteration(n):
+    count = 0
+    while n > 1.0:
+       if n % 2 == 0:
+        n = int(n / 2)
+        count = count + 1
+       else:
+        n = int(3 * n + 1)
+        count = count + 1
+    
+    print("The loop was executed ", count, "times. ")
+    return None
+
