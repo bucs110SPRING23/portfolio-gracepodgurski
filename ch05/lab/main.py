73dff88b43efc160ac870a8162cade0a91eaa3d7
gracepodgurski <gracepodgurski@yahoo.com>
2023-03-20 22:12:42 -0400

    last one

--- ch05/lab/main.py
+++ ch05/lab/main.py
@@ -1,5 +1,6 @@
 import pygame
 
+# Part A
 def iteration(n):
     count = 0
     while n > 1.0:
@@ -17,15 +18,33 @@ def iteration_range(upper_limit):
   objs_in_seq = {}
   for n in range(2,upper_limit+1):
     iteration(upper_limit)
-    objs_in_seq.update({count:upper_limit})
+    objs_in_seq.update({n:iteration(n)})
+  print(objs_in_seq)
   return objs_in_seq
 
+# Part B
+def graphing_iterations(objs_in_seq):
+  while 1:
+    pygame.event.pump()
+    screen = pygame.display.set_mode()
+    coordinates = objs_in_seq.items()
+    print(coordinates)
+    pygame.draw.lines(screen,"blue",True,coordinates)
+    new_display = pygame.transform.flip(screen, False, True)
 
+    width, height = new_display.get_size()
+    new_display = pygame.transform.scale(new_display, (width * 5, height * 5))
+
+    screen.blit(new_display, (0, 0))
+
+
+
+# --- MAIN CODE ---
 def main():
   pygame.init()
-  # iteration(25)
+  iteration(5)
   iteration_range(20)
-  print(objs_in_seq)
+  graphing_iterations(10)
   return None 
 
 main()
--- ch06/midterm/README.md
+++ ch06/midterm/README.md
@@ -2,8 +2,8 @@
 
 ## SHORT DESCRIPTION: This program allows the user to draw a flower of the color of their choice
 
-## KNOWN BUGS AND INCOMPLETE PARTS *(list any known bugs or non-working parts)*
+## KNOWN BUGS AND INCOMPLETE PARTS: Possibly not meeting the return requirements for functions
 
 ## REFERENCES: Used this website for tips for drawing the petals https://www.tutorialspoint.com/turtle-programming-in-python
 
-## MISCELLANEOUS COMMENTS *(anything else you would like the grader to know)*
+## MISCELLANEOUS COMMENTS: I hope you enjoy!
