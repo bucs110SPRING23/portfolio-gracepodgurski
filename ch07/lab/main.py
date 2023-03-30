0e1afc6858d799ce1f45f3bb669a6f694fa879dd
gracepodgurski <gracepodgurski@yahoo.com>
2023-03-30 11:53:21 -0400

    saving

---
+++ ch07/lab/main.py
@@ -0,0 +1,11 @@
+
+r = Rectangle(10, 10, 10, 10)
+assert((r.x, r.y, r.height, r.width) == (10,10,10,10))
+r = Rectangle(-1, 1, 1, 1)
+assert((r.x, r.y, r.height, r.width) == (1,1,1,1))
+r = Rectangle(1, -5, 1, 1)
+assert((r.x, r.y, r.height, r.width) == (1,5,1,1))
+r = Rectangle(1, 1, -10, 1)
+assert((r.x, r.y, r.height, r.width) == (1,1,10,1))
+r = Rectangle(1, 1, 1, -1000)
+assert((r.x, r.y, r.height, r.width) == (1,1,1,1000))
