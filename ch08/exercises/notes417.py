8ee9903ce6ca1f8d5aa6d6e06ce510fdf19acb4e
gracepodgurski <gracepodgurski@yahoo.com>
2023-04-17 16:55:25 -0400

    save

---
+++ ch08/exercises/notes417.py
@@ -0,0 +1,9 @@
+# More notes - kind of a part 2 to the other notes file
+# In a GUI, everything will be in classes
+
+## Controller - like  a model, label for how a class is used
+
+# Model - data
+# View - display (pygame)
+# Controller - logic (director)
+
