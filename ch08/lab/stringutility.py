e0a959656303d36fe6c8aa3de8dce31a5df3505d
gracepodgurski <gracepodgurski@yahoo.com>
2023-04-20 13:06:46 -0400

    lab

--- ch08/lab/stringutility.py
+++ ch08/lab/stringutility.py
@@ -6,7 +6,7 @@ class StringUtility:
             my_vowels =['a','e','i','o','u','A','E','I','O','U']
             count = 0
             i = 0 
-            for i in range(len(test_strings))
+            for i in range(len(test_strings)):
                 if test_strings[i] in my_vowels:
                     count += 1
             print("Number of vowels in the string is:")
