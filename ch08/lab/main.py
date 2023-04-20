b8c5dfdff7f0f30e09dc947c1d6b7b5bba914b13
gracepodgurski <gracepodgurski@yahoo.com>
2023-04-20 11:43:22 -0400

    hi

--- ch08/lab/main.py
+++ ch08/lab/main.py
@@ -10,7 +10,6 @@ def main():
     for i in test_strings:
         su.append(StringUtility(i))
 
-
     print("=========== Testing __str__ method... ===========")
     expected_results = ["interesting", "aardvark", "aaa", "aeiouAEIOU", "a b c d e f g h i j k l m n o p q r s t u v w x y z", '']
     i = 0
