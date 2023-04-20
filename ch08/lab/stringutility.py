599ada184bf060df4fe251536e24f50dbb153549
gracepodgurski <gracepodgurski@yahoo.com>
2023-04-20 12:40:24 -0400

    part 1

--- ch08/lab/stringutility.py
+++ ch08/lab/stringutility.py
@@ -1,3 +1,12 @@
-def __init__(self,string):
-    pass
+class StringUtility:
+    def __init__(self,string):
+        pass
     
+        def vowels(self):
+            my_vowels =['a','e','i','o','u','A','E','I','O','U']
+            count = 0
+            i = 0 
+            for i in range(len(test_strings))
+                if test_strings[i] in my_vowels:
+                    count += 1
+            print("Number of vowels in the string is:")
