0ccf79480b0ce258a4c1b8525cf0b12c05837aa1
gracepodgurski <gracepodgurski@yahoo.com>
2023-04-24 11:24:13 -0400

    saving progress

--- ch08/lab/stringutility.py
+++ ch08/lab/stringutility.py
@@ -2,11 +2,30 @@ class StringUtility:
     def __init__(self,string):
         pass
     
-        def vowels(self):
-            my_vowels =['a','e','i','o','u','A','E','I','O','U']
-            count = 0
-            i = 0 
-            for i in range(len(test_strings)):
-                if test_strings[i] in my_vowels:
-                    count += 1
-            print("Number of vowels in the string is:")
+    def vowels(self):
+        my_vowels =['a','e','i','o','u','A','E','I','O','U']
+        count = 0
+        i = 0 
+        for i in range(len(test_strings)):
+            if test_strings[i] in my_vowels:
+                count += 1
+        if count < 5:
+            return str(count)
+        elif count >= 5:
+            return "many"
+
+        print("Number of vowels in the string is:",count)
+
+    def bothEnds(self):
+        for i in range(len(test_strings)):
+            if len(i) > 2:
+                return str(test_strings[0]), str(test_strings[1]), str(test_strings[len(test_strings-2)]), str(test_strings[len(test_strings-1)])
+            elif len(i)<=2:
+                return " "
+            
+    def fixStart(self):
+        for i in test_strings:
+        # Find a way to make this not alter the first letter
+            if str(i) = str(test_strings[0]):
+                append.test_strings(i='*')
+
