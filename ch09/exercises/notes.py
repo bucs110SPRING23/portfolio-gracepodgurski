7c74e80e539dab4ee19e37aea1f36ad721a09861
gracepodgurski <gracepodgurski@yahoo.com>
2023-04-26 17:19:24 -0400

    save

--- ch09/exercises/notes.py
+++ ch09/exercises/notes.py
@@ -24,5 +24,36 @@
         # status codes (200  =  everything went fine, 400 = resource can not be delivered, 500  = bad code on server)
         # IP  addresses
         # system info (geolocation)
-# 
+
+
+## urllib 
+
+# Request library - "Requests: HTTPs for  Humans"
+
+# API = Application Programmers Interface
+#  - Use an API to make a request to the server in the expected format
+#  APIs can return data in any format, usually returned in JSON
+
+# URL parameters: ? , &
+    # binghamton.edu
+    # binghamton.edu/cs
+    # binghamton.edu/cs?  - ? has variables after it which can be changed
+    # binghamton.edu/cs?var1=100&var2=300
+
+# opentdb.com - open trivia base 
+    # we want to use the API so we will have to look up the documentation 
+
+    # opentdb.com/api.php?amount=1&category=18
+        # this gives us one question from category 18 (comp sci)
+
+
+import requests
+
+def main():
+    response = requests.get("https://opentdb.com/api.php?amount=1&category=18")
+    print(response.status_code)
+    print(response.text)
+
+
+main()
 
