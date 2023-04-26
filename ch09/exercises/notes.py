bb1273d4b2688f12b9872721cff6b0bd4313a0f2
gracepodgurski <gracepodgurski@yahoo.com>
2023-04-26 16:59:49 -0400

    in class notes!

--- ch09/exercises/notes.py
+++ ch09/exercises/notes.py
@@ -2,3 +2,27 @@
 
 # for final project - have a assets folder, src folder - with controller, model, etc folder, main.py file & menu.py 
 # GUI menus and extensions
+
+### 4/26/2023 - Requests and APIs - Topic for final exam 
+
+# Network Programming 
+#  - Build a program that asks trivia questions 
+
+# Server: any computer listening for incoming network connections 
+# Request: incoming connection that asks for some resource from the server
+# Resources can  be images, videos, music, media files, text, JSON, HTML 
+
+# Types of requests 
+    # GET - read 
+        # visiting a page, downloading a file
+    # PUT - write operation (requires more security)
+        # login, deleting 
+
+# Headers 
+# - sent with the request and the response 
+# -  tells you what's happening  -  info about the request 
+        # status codes (200  =  everything went fine, 400 = resource can not be delivered, 500  = bad code on server)
+        # IP  addresses
+        # system info (geolocation)
+# 
+
