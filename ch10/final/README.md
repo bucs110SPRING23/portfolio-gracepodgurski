4f3c55e4770bab0c585693a0645f73fe2c665a7f
gracepodgurski <gracepodgurski@yahoo.com>
2023-05-08 13:41:15 -0400

    final!

--- ch10/final/README.md
+++ ch10/final/README.md
@@ -1,9 +1,14 @@
 # CS 110 Final Exam
 
-# Description
+# Description: Goal of this project is to have a random emoji generated in the category of animals and nature and use this result to generate a poem with this item in the title 
 
-# Known bugs or incomplete parts
+# Known bugs or incomplete parts: I attempted to connect the two, but had some difficulty. The poetry section has not worked successfully, as the string returned from the emoji section did not successfully transfer to the title 
 
-# References
+# References:
+https://www.freecodecamp.org/news/python-substring-how-to-slice-a-string/
+https://github.com/cheatsnake/emojihub
+https://github.com/thundercomb/poetrydb#readme
+
 
 # MISC comments
+Thank you for an amazing semester! While I struggle a lot with coding, this class made me understand the fundamentals a little better & made learning fun. 
--- ch10/final/main.py
+++ ch10/final/main.py
@@ -1,30 +1,44 @@
 import requests
+import random
 
-class Astrology:
+class Emoji:
     def __init__(self):
-        self.url = "https://aztro.sameerkumar.website?"
-        self.additions = "sign=gemini &day= today"
+        self.url = "https://emojihub.yurace.pro/api/random"
+        self.additions = "/category/animals-and-nature"
 
     def get(self):
-        url = self.url
+        url = self.url + self.additions
+        response = requests.get(url)
+        print(response.status_code)
+        print(response.text)
+        # data = response.json()
+        # return data['results']
+
+class Poetry:
+    def __init__(self):
+        self.url = "https://poetrydb.org/title/"
+        self.title = ""
+
+    def get(self):
+        url = self.url + self.title
         response = requests.get(url)
         data = response.json()
         return data['results']
     
 
 def main():
-    astro = Astrology()
+    my_emoji = Emoji()
+    results = requests.get("https://emojihub.yurace.pro/api/random/category/animals-and-nature")
+    flora_fauna = str(results.text)
+    my_plantoranimal = flora_fauna.split()
 
-    # params = (('sign', 'gemini'),('day', 'today'),)
+    for i in enumerate(my_plantoranimal):
+        object = str(i)
+        print(object)
 
-    # requests.post('https://aztro.sameerkumar.website/', params=params)
-
-    results = astro.get()
-    print(results.status_code)
-    print(results.text)
-
-
-
-
+    Poetry.self.title.append(object)
+    my_poem = Poetry()
+    final = requests.get("https://poetrydb.org/title/"+object)
 
 main()
+
